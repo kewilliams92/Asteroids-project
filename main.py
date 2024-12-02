4bc0907320f7359a5f72377ac37db2687fad4c66
kewilliams92 <kewilliamsdev@gmail.com>
2024-12-02 17:03:43 -0500

    Added player shapes and movement

--- main.py
+++ main.py
@@ -4,18 +4,28 @@
 import pygame
 
 from constants import *
+from player import Player
 
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill(000000)
+        player.update(dt)
+        screen.fill((0, 0, 0))
+        player.draw(screen)
+        # the game will rerender here
         pygame.display.flip()
+        # limit the game to 60 frames per second
+        dt = clock.tick(60) / 1000
+
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
